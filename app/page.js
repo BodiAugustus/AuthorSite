d77f06b78c0fbf9d5d0710ffd644138df3f86498
BodiAugustus <tristanettles@gmail.com>
2025-07-10 08:01:44 -0400

    SEO

--- app/page.js
+++ app/page.js
@@ -4,8 +4,27 @@ import ComingSoon from "./ComingSoon/page";
 import NewsletterSignup from "./NewsLetterSignup/page";
 
 export default function Home() {
+  const structuredData = {
+    "@context": "https://schema.org",
+    "@type": "Person",
+    name: "Tristan Nettles",
+    url: "https://www.tristannettles.com",
+    image: "https://www.tristannettles.com/preview.png",
+    sameAs: ["https://twitter.com/tristan_nettles"],
+    jobTitle: "Novelist",
+    description:
+      "Tristan Nettles is the author of historical and Bronze Age fiction including 'The Shepherd' and 'False Positive'.",
+    worksFor: {
+      "@type": "Organization",
+      name: "Ashley DeFi",
+    },
+  };
   return (
     <div>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
+      />
       <HomePage />
       <NewReleases />
       <ComingSoon />
