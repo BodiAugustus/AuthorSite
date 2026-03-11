fe28efa72c84a37b18904beefe825b40e412241f
BodiAugustus <tristanettles@gmail.com>
2026-03-11 10:38:28 -0400

    add edits

--- app/upcoming-works/page.js
+++ app/upcoming-works/page.js
@@ -78,7 +78,7 @@ export default function UpcomingWorks() {
             <p className="text-gray-800 italic">{book.available}</p>
             <a
               href={book.website}
-              className="mt-4 px-4 py-2 bg-black text-white rounded hover:scale-105 active:scale-105 transition-all"
+              className="mt-4 px-4 py-2 bg-black text-white rounded shadow-md hover:scale-105 hover:shadow-xl active:translate-y-1 active:shadow-inner active:scale-95 transition-all duration-150 ease-in-out"
             >
               Pre-Order Now!
             </a>
